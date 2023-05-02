d1161e55e5fc37129443457f6ed2ac4b986b15a5
jose alejandro <josealejandrocc22@gmail.com>
2023-05-01 21:56:20 -0500

    sockets

--- src/socket.js
+++ src/socket.js
@@ -2,6 +2,6 @@ import { io } from "socket.io-client";
 
 // importaciones y constantes de socket
 // const URL = "http://localhost:4000/";
-const URL = "apiquizapp-production.up.railway.app";
+const URL = "apiquizapp-production.up.railway.app/";
 
 export const socket = io(URL)
