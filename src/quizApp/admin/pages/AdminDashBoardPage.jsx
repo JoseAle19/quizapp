19a09ea3caa4b322ec7a006bb0c91a4bdf44078c
jose alejandro <josealejandrocc22@gmail.com>
2023-05-02 00:17:13 -0500

    Examen admin no disponible

--- src/quizApp/admin/pages/AdminDashBoardPage.jsx
+++ src/quizApp/admin/pages/AdminDashBoardPage.jsx
@@ -1,11 +1,22 @@
 import { useEffect} from "react";
+// importaciones de redux
 import { useDispatch, useSelector } from "react-redux";
+// Slice de test para obtener los examenes
 import { getTest } from "../../../store/slices/testSlice/thunks";
+// custom hooks convertir json
 import { custom_hook_jsons } from "../../global_hooks/custom_hook_jsons";
+// Componentes es el de cargando
 import { Loading } from "../../../ui/components/Loading";
+//UseNavigate
 import { useNavigate } from "react-router-dom";
+
+
+// variables globales especial de socket
 import { socket } from "../../../socket";
 
+
+// Alertas
+import Swal from "sweetalert2";
 export const AdminDashBoardPage = () => {
   const { tests, isLoading } = useSelector((state) => state.test);
   const dispatch = useDispatch();
@@ -17,6 +28,18 @@ export const AdminDashBoardPage = () => {
   }, []);
   // CustomHook para parsear jsons
   const { getStorage, parseJson } = custom_hook_jsons();
+
+//? Alerta para que aun no muestre el examen
+  const alertForTest = () => {
+    Swal.fire({
+      title: "El examen aun no esta disponible",
+      text: "El examen aun no esta disponible",
+      icon: "warning",
+      confirmButtonText: "Ok",
+    });
+  }
+
+
   return (
     <>
       <div className="Leadertest-test">
@@ -51,7 +74,9 @@ export const AdminDashBoardPage = () => {
 
                 <button 
                 onClick={() => {
-                  navigate(`/questions-test/${test.id}`)
+                  // TODO: ya se puede ver el examen
+                    alertForTest()
+                  // navigate(`/questions-test/${test.id}`)
                 }}
                 className="Leadertest-btn">Ver examen</button>
               </div>
--- src/store/slices/categoriesQuiz/thunks.js
+++ src/store/slices/categoriesQuiz/thunks.js
@@ -5,8 +5,6 @@ export const getCategories = () => {
     return async (dispatch) => {
         dispatch(startLoading())
         
-        //Peticion al api hacia  el endpoint de categorias
-        // endPoint de llas categorias http://localhost:4000/categories/getAllCategories
         const  data = await getCategoriesService()
         dispatch(setCategories(data))
   };
